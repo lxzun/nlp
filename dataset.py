103963e185e5cf68c4d95c0fe22cf67f29e33d6a
lxzun <cpfl410@gmail.com>
2021-05-16 14:30:09 +0900

    update BERT

--- dataset.py
+++ dataset.py
@@ -1,20 +1,40 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
+from torch import nn
 
-def load_datasets(task='pretrain'):
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
-    if task == 'pretrain':
-        data = load_dataset('openwebtext')
-        print('load Done!')
-        data = data.map(pretrain_encode(tokenizer, data), batched=True)
-        print('encode Done!')
-        data = data.set_format(type='torch', columns=['text'])
-        print('set format')
-    return data
+class Mydataset(nn.Module):
+    def __init__(self, task='pretrain', max_length=1025, split='train', return_attention_mask=False, return_token_type_ids=False):
+        super(Mydataset, self).__init__()
+        self.return_attention_maks = return_attention_mask
+        self.return_token_type_ids = return_token_type_ids
+        self.max_length = max_length
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        self.vocab_size = self.tokenizer.vocab_size
+        self.pad_ids = self.tokenizer.pad_token_id
+        self.task = task
+        if task == 'pretrain':
+            self.data = load_dataset('openwebtext', split='train')
+            self.length = self.data.num_rows
+            if split == 'train':
+                self.length = int(self.length*0.9)
+                self.data = self.data[:self.length]
+            else:
+                self.length = int(self.length*0.9)
+                self.data = self.data[self.length:]
+            print('load Done!')
 
-def pretrain_encode(tokenizer, data):
-    return tokenizer(data["text"], truncation=True, padding='max_length')
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, item):
+        if self.task == 'pretrain':
+            data = self.tokenizer(self.data[item]["text"], truncation=True, padding='max_length',
+                                  max_length=self.max_length, return_tensors='pt', return_attention_mask=False,
+                                  return_token_type_ids=False)['input_ids']
+            data = data[:, 1:].flatten()
+            return data, data
+
+
 
 if __name__=='__main__':
-    a = load_datasets()
-    print('done!')
+    print()
--- train.py
+++ train.py
@@ -1,11 +1,48 @@
-from torch import cuda, optim
+from torch import optim, nn
 from torch.utils.data import DataLoader
 import torch
-import datetime
+from datetime import datetime
 import os
 import argparse
 from tensorboardX import SummaryWriter
-from dataset import load_datasets
+from dataset import Mydataset
+from models.Mymodel import Mymodelforpretrain
+
+
+def train(model, trainloader, criterion, optimizer, epoch_idx, args, device):
+    avg_loss = 0
+    model.train()
+
+    for batch_idx, (data, labels) in enumerate(trainloader, 1):
+
+        imgs, labels = data.to(device), labels.to(device)
+        logit = model(data)
+        loss = criterion(logit, labels)
+        loss.backward()
+
+        avg_loss += loss / num_batchs
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch_idx % args.step_batch == 0:
+            log('epoch: {:2d}/{}  |  batch: {:2d}/{}  |  loss: {:.4f}'.format(
+                epoch_idx, args.num_epochs,
+                batch_idx, num_batchs,
+                loss.item()))
+    return avg_loss
+
+
+def evaluation(model, testloader, criterion, device):
+    avg_loss = 0
+    model.test()
+    with torch.no_grad:
+        for batch_idx, (data, labels) in enumerate(testloader, 1):
+            imgs, labels = data.to(device), labels.to(device)
+            logit = model(data)
+            loss = criterion(logit, labels)
+            avg_loss += loss / num_batchs
+    return avg_loss
+
 
 def mk_dir(path_: str):
     os.makedirs(path_) if not os.path.isdir(path_) else None
@@ -24,7 +61,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--num_epochs', type=int, default=2000)
     parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--step_batch', type=int, default=1)
+    parser.add_argument('--step_batch', type=int, default=1000)
     parser.add_argument('--eval_batch_size', type=int, default=128)
 
     parser.add_argument('--lr', type=float, default=5e-05)
@@ -33,11 +70,11 @@ if __name__ == '__main__':
 
     parser.add_argument('--hidden_size', type=int, default=768)
     parser.add_argument('--m', type=int, default=24)
-    parser.add_argument('--out_dim', type=int, default=50)
+    parser.add_argument('--out_dim', type=int, default=128)
     parser.add_argument('--k', type=int, default=3)
+    parser.add_argument('--n_layer', type=int, default=12)
     parser.add_argument('--task', type=str, default='pretrain')
 
-    parser.add_argument('--save_vocab_path', type=str, default='')
     parser.add_argument('--save_vocab', type=bool, default=True)
     parser.add_argument('--pretrained_vocab_path', type=str, default='', help='load pretrained vocab path')
     parser.add_argument('--pretrained_vocab', type=bool, default=False, help='load pretrained vocab')
@@ -69,3 +106,43 @@ if __name__ == '__main__':
         log('{}: {}'.format(k, v))
     log('Real used device: {}'.format(device))
 
+    trainset = Mydataset(task=args.task)
+    testset = Mydataset(task=args.task, split='test')
+    trainloader = DataLoader(trainset, args.batch_size, num_workers=2, shuffle=True)
+    testloader = DataLoader(testset, args.batch_size, num_workers=2, shuffle=True)
+
+    num_batchs = len(trainloader)
+    log('\n---- dataset info ----')
+    log('\n* train data *')
+    log('- num : {}'.format(len(trainloader.dataset)))
+    log('\n* eval data *')
+    log('- num : {}'.format(len(testloader.dataset)))
+    log('----------------------\n')
+
+    model = Mymodelforpretrain(args.m, args.out_dim, args.hidden_size, trainset.vocab_size, args.n_layer, trainset.pad_ids)
+
+    if args.multi_gpu and torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    optimizer.zero_grad()
+
+    best_loss = 9999
+    best_epoch = 0
+
+    for epoch_idx in range(1, args.num_epochs + 1):
+        log('\n\n----------------------- {} epoch start! -----------------------'.format(epoch_idx))
+        avg_loss = train(model, trainloader, criterion, optimizer, epoch_idx, args, device)
+        eval_loss, eval_acc = evaluation(model, testloader, criterion, device)
+        log(' >> epoch: {:2d}\t|\tavg_loss: {:.4f}\t|\teval_loss: {:.4f}\t'.format(
+            epoch_idx, avg_loss, eval_loss))
+
+        writer.add_scalars('loss', {'train loss': avg_loss, 'eval loss': eval_loss}, epoch_idx)
+
+        if best_loss > eval_loss:
+            best_loss = eval_loss
+            best_epoch = epoch_idx
+            writer.add_text('best loss', '{}e_{:.4f}%'.format(best_epoch, best_loss), epoch_idx)
+
+    writer.close()
